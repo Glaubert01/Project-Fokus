a4a7a63a4b1bf4080b6e193f5ae0fea70fcb65cc
Glaubert Nunes <glaubert@outlook.com>
2023-11-07 16:42:30 -0300

    adicionado o som

--- script.js
+++ script.js
@@ -4,24 +4,47 @@ const curtoBt = document.querySelector('.app__card-button--curto');
 const longoBt = document.querySelector('.app__card-button--longo');
 const banner = document.querySelector('.app__image');
 const titulo = document.querySelector('.app__title');
+const botoes = document.querySelectorAll('.app__card-button');
+const startPauseBt = document.querySelector('#start-pause');
+const musicaFocoInput = document.querySelector('#alternar-musica');
+const musica = new Audio('/Sons/luna-rise-part-one.mp3');
+musica.loop = true;
 
+let tempoDecorridoEmSegundos = 5;
+let intervaloId = null;
+
+musicaFocoInput.addEventListener('change', () => {
+    if(musica.paused) {
+        musica.play();
+    } else {
+        musica.pause();
+    }
+})
+   
 focoBt.addEventListener('click', () => {
     alterarContexto('foco');
+    focoBt.classList.add('active');
 })
 
 curtoBt.addEventListener('click', () => {
     alterarContexto('descanso-curto');
-
+    curtoBt.classList.add('active');
 })
 
 longoBt.addEventListener('click', () => {
     alterarContexto('descanso-longo');
-
+    longoBt.classList.add('active');
 })
 
 function alterarContexto (contexto) {
+
+    botoes.forEach(function (contexto){
+        contexto.classList.remove('active');
+    })
+    
     html.setAttribute('data-contexto', contexto);
     banner.setAttribute('src', `/Imagens/${contexto}.png`);
+    
     switch(contexto) {
         case 'foco':
             titulo.innerHTML = `Otimize sua produtividade,
@@ -36,6 +59,27 @@ function alterarContexto (contexto) {
             <strong class="app__title-strong">Faça uma pausa longa.</strong> `;
             break;
     }
-
 }
 
+function contagemRegressiva = () => {
+    if(tempoDecorridoEmSegundos <= 0){
+        zerar();
+        alert('Tempo finalizado!')
+        return;
+    }
+    tempoDecorridoEmSegundos -= 1;
+}
+
+startPauseBt.addEventListener('click', iniciarOuPausar);    
+
+function iniciarOuPausar(){
+    if(intervaloId){
+        zerar();
+    }
+    intervaloId = setInterval(contagemRegressiva, 1000);
+}
+
+function zerar() {
+    clearInterval(intervaloId);
+    intervaloId = null;
+}
